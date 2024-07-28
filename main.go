65ac98d95aef8132d0c3b1d850f32ef2dfcae477
Prajwal Koirala <102563715+Prajwal-Koirala@users.noreply.github.com>
2024-07-28 19:19:31 -0400

    Update main.go
    Signed-off-by: Prajwal Koirala <102563715+Prajwal-Koirala@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,23 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"json"
+	"io/ioutil"
 	"log"
 	"os/exec"
+	"runtime"
 )
 
 func main() {
 	// Check if the application is installed.
-	checkApplication("yt-dlp")
+	fmt.Println(checkApplication("yt-dlp"))
 }
 
 // Check if an application is installed
 func checkApplication(appName string) bool {
 	// Check if a given application is running.
-	cmd := exec.Command(appName)
-	err := cmd.Run()
+	_, err := exec.LookPath(appName)
 	if err != nil {
 		log.Println(err)
 	}
+	return err == nil
+}
+
+// Get the name of the operating system.
+func operatingSystemName() string {
+	return runtime.GOOS
+}
+
+// Read the json file and return it as a string slice.
+func readJSONFile(filePath string) []string {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Println(err)
+	}
+	// Declare a slice to hold the parsed data
+	var urls []string
+	// Unmarshal the JSON data into the slice
+	err = json.Unmarshal(data, &urls)
+	if err != nil {
+		log.Println(err)
+	}
+	return urls
 }
